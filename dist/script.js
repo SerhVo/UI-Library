2a6ae41ad725e62a8b659f1b13e5f83501448ebf
Serhii Vorotnoi <svorotnoy@gmail.com>
2024-11-14 13:26:17 +0200

    step11: carousel

--- dist/script.js
+++ dist/script.js
@@ -28,6 +28,157 @@ _core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.accordion = function (he
 
 /***/ }),
 
+/***/ "./src/js/lib/components/carousel.js":
+/*!*******************************************!*\
+  !*** ./src/js/lib/components/carousel.js ***!
+  \*******************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../core */ "./src/js/lib/core.js");
+// import $ from "../core";
+
+// $.prototype.carousel = function () {
+//   for (let i = 0; i < this.length; i++) {
+//     const width = window.getComputedStyle(
+//       this[i].querySelector(".carousel-inner")
+//     ).width;
+//     const slides = this[i].querySelectorAll(".carousel-item");
+//     const slidesField = this[i].querySelector(".carousel-slides");
+//     const dots = this[i].querySelectorAll(".carousel-indicators li");
+
+//     slidesField.style.width = 100 * slides.length + "%";
+//     slides.forEach((slide) => {
+//       slide.style.width = width;
+//     });
+
+//     let offset = 0;
+//     let slideIndex = 0;
+
+//     $(this[i].querySelector('[data-slide="next"]')).click((e) => {
+//       e.preventDefault();
+//       if (offset == +width.replace(/\D/g, "") * (slides.length - 1)) {
+//         offset = 0;
+//       } else {
+//         offset += +width.replace(/\D/g, "");
+//       }
+
+//       slidesField.style.transform = `translateX(-${offset}px)`;
+
+//       if (slideIndex == slides.length - 1) {
+//         slideIndex = 0;
+//       } else {
+//         slideIndex++;
+//       }
+//       dots.forEach((dot) => dot.classList.remove("active"));
+//       dots[slideIndex].classList.add("active");
+//     });
+
+//     $(this[i].querySelector('[data-slide="prev"]')).click((e) => {
+//       e.preventDefault();
+//       if (offset == 0) {
+//         offset = +width.replace(/\D/g, "") * (slides.length - 1);
+//       } else {
+//         offset -= +width.replace(/\D/g, "");
+//       }
+
+//       slidesField.style.transform = `translateX(-${offset}px)`;
+
+//       if (slideIndex == 0) {
+//         slideIndex = slides.length - 1;
+//       } else {
+//         slideIndex--;
+//       }
+//       dots.forEach((dot) => dot.classList.remove("active"));
+//       dots[slideIndex].classList.add("active");
+//     });
+
+//     const sliderId = this[i].getAttribute("id");
+//     $(`#${sliderId} .carousel-indicators li`).click((e) => {
+//       const slideTo = e.target.getAttribute("data-slide-to");
+
+//       slideIndex = slideTo;
+//       offset = +width.replace(/\D/g, "") * slideTo;
+
+//       slidesField.style.transform = `translateX(-${offset}px)`;
+//       dots.forEach((dot) => dot.classList.remove("active"));
+//       dots[slideIndex].classList.add("active");
+//     });
+//   }
+// };
+
+// $(".carousel").carousel();
+
+//  ----------    Рефакторинг кода  ----
+
+
+
+//  функциональность карусели для всех выбранных элементов
+_core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.carousel = function () {
+  Array.from(this).forEach(carousel => {
+    // Получаем ширину одного слайда
+    const width = parseInt(window.getComputedStyle(carousel.querySelector(".carousel-inner")).width);
+    const slides = carousel.querySelectorAll(".carousel-item");
+    const slidesField = carousel.querySelector(".carousel-slides");
+    const dots = carousel.querySelectorAll(".carousel-indicators li");
+
+    // Устанавливаем ширину поля для слайдов, чтобы вместить все слайды в одну строку
+    slidesField.style.width = `${100 * slides.length}%`;
+
+    // Задаем ширину каждого слайда в соответствии с шириной внутренней части карусели
+    slides.forEach(slide => slide.style.width = `${width}px`);
+    let offset = 0;
+    let slideIndex = 0;
+
+    // Функция для обновления позиции слайдов и активной точки, исходя из текущего индекса
+    const updateSlidePosition = () => {
+      slidesField.style.transform = `translateX(-${offset}px)`;
+      dots.forEach(dot => dot.classList.remove("active"));
+      dots[slideIndex].classList.add("active");
+    };
+
+    // Переход к следующему слайду и обновление смещения
+    const moveNext = () => {
+      slideIndex = (slideIndex + 1) % slides.length;
+      offset = width * slideIndex;
+      updateSlidePosition();
+    };
+
+    // Переход к предыдущему слайду и обновление смещения
+    const movePrev = () => {
+      slideIndex = (slideIndex - 1 + slides.length) % slides.length;
+      offset = width * slideIndex;
+      updateSlidePosition();
+    };
+
+    // Добавляем обработчик события на кнопку "next" для перехода к следующему слайду
+    (0,_core__WEBPACK_IMPORTED_MODULE_0__["default"])(carousel.querySelector('[data-slide="next"]')).click(e => {
+      e.preventDefault();
+      moveNext();
+    });
+
+    // Добавляем обработчик события на кнопку "prev" для перехода к предыдущему слайду
+    (0,_core__WEBPACK_IMPORTED_MODULE_0__["default"])(carousel.querySelector('[data-slide="prev"]')).click(e => {
+      e.preventDefault();
+      movePrev();
+    });
+
+    // Добавляем обработчики событий для каждой точки, чтобы при клике переходить к соответствующему слайду
+    dots.forEach((dot, index) => {
+      dot.addEventListener("click", () => {
+        slideIndex = index;
+        offset = width * slideIndex;
+        updateSlidePosition();
+      });
+    });
+  });
+};
+
+// Инициализируем функциональность карусели для всех элементов с классом "carousel"
+(0,_core__WEBPACK_IMPORTED_MODULE_0__["default"])(".carousel").carousel();
+
+/***/ }),
+
 /***/ "./src/js/lib/components/dropdown.js":
 /*!*******************************************!*\
   !*** ./src/js/lib/components/dropdown.js ***!
@@ -213,6 +364,8 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _components_modal__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! ./components/modal */ "./src/js/lib/components/modal.js");
 /* harmony import */ var _components_tab__WEBPACK_IMPORTED_MODULE_9__ = __webpack_require__(/*! ./components/tab */ "./src/js/lib/components/tab.js");
 /* harmony import */ var _components_accordion__WEBPACK_IMPORTED_MODULE_10__ = __webpack_require__(/*! ./components/accordion */ "./src/js/lib/components/accordion.js");
+/* harmony import */ var _components_carousel__WEBPACK_IMPORTED_MODULE_11__ = __webpack_require__(/*! ./components/carousel */ "./src/js/lib/components/carousel.js");
+
 
 
 
